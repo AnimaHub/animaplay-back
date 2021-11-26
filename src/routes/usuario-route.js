b349a5799df6311b3050efdd8e938891cf0f7012
lohannalucia <lohannalucia55@gmail.com>
2021-11-25 22:10:11 -0300

    add password recovery endpoints
    - fix "vaalidator" to "validator";
    - create mailservice to send account recovery link using nodemailer*;
    - add endpoint to put user password;
    - add endpoint to sendmail using mailservice;

--- src/routes/usuario-route.js
+++ src/routes/usuario-route.js
@@ -1,16 +1,19 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controler=require('../controler/usuario-controler');
-const authService=require('../services/auth-service');
-const tiposAuthorization = require('../services/types-authorization');
+const controler = require("../controler/usuario-controler");
+const authService = require("../services/auth-service");
+const tiposAuthorization = require("../services/types-authorization");
 
 // CRUD
-router.post('/', controler.post);
+router.post("/", controler.post);
 
-router.put('/', tiposAuthorization.todas, authService.authorize, controler.put);
+router.put("/", tiposAuthorization.todas, authService.authorize, controler.put);
 
-router.post('/login', controler.login);
+router.post("/login", controler.login);
 
-module.exports=router;
+router.post("/password/recovery", controler.sendMailToResetPassword);
+router.put("/password/new", tiposAuthorization.todas, authService.authorize, controler.updatePassword);
+
+module.exports = router;
