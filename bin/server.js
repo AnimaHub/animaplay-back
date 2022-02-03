658df5ea0acca5db1073b4cd46335a92ebd68e57
lohanna54 <lohannalucia55@gmail.com>
2022-02-02 20:56:08 -0600

    [FIX] default value for service running port

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,7 @@ const app=require('../src/app')
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 
-const port = normalizePort(process.env.port || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
